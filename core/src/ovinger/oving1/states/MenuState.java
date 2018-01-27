c3928c25c6b9485510ec4eb63b75f5b821eb934d
Quint-V <vicweiwan@gmail.com>
2018-01-27 23:14:01 +0100

    ASSIGNMENT COMPLETED  - minor cleanup in code, no real changes

--- core/src/ovinger/oving1/states/MenuState.java
+++ core/src/ovinger/oving1/states/MenuState.java
@@ -1,13 +1,10 @@
 package ovinger.oving1.states;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
 import ovinger.oving1.GameDemo;
 
 /**
@@ -29,7 +26,7 @@ public class MenuState extends State{
     @Override
     protected void handleInput() {
         if (Gdx.input.justTouched()) {
-            if (Gdx.input.getY() < GameDemo.HEIGHT*1/3){
+            if (Gdx.input.getY() < GameDemo.HEIGHT/3){
                 gsm.set(new Task_1_2_state(gsm));
                 System.out.println("Tasks 1, 2 running");
             }
@@ -55,14 +52,14 @@ public class MenuState extends State{
         shapeRenderer.setProjectionMatrix(cam.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(new Color(0.5f, 0, 0, 0.1f));   // Red
-        shapeRenderer.rect(0, GameDemo.HEIGHT/3*2, GameDemo.WIDTH*1, GameDemo.HEIGHT/3);
+        shapeRenderer.rect(0, GameDemo.HEIGHT/3*2, GameDemo.WIDTH, GameDemo.HEIGHT/3);
         shapeRenderer.setColor(new Color(0,.5f,0, 0.1f)); // Green
         shapeRenderer.rect(0, GameDemo.HEIGHT/3, GameDemo.WIDTH, GameDemo.HEIGHT/3);
         shapeRenderer.setColor(new Color(0.2f, 0.32f, 0.62f, 0.1f));    // Blue
         shapeRenderer.rect(0, GameDemo.HEIGHT, GameDemo.WIDTH, GameDemo.HEIGHT/3);
         shapeRenderer.end();
         sb.begin();
-        text.draw(sb, "Press R to reset", GameDemo.WIDTH*1/5,GameDemo.HEIGHT*0.5f);
+        text.draw(sb, "Press R to reset", GameDemo.WIDTH/5,GameDemo.HEIGHT*0.5f);
         text.draw(sb, "Tasks 1 & 2", cam.position.x,GameDemo.HEIGHT*5/6);
         text.draw(sb, "Task 3", cam.position.x, GameDemo.HEIGHT*3f/6);
         text.draw(sb, "Pong", cam.position.x,GameDemo.HEIGHT*1f/6);
